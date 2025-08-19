b5f8f177739e97cf45a44958f0a51c57701ab348
Ayaan Arif Nadeem <socialayaan@gmail.com>
2025-08-19 20:54:43 +0500

    Punch animation and code

--- player.cpp
+++ player.cpp
@@ -8,29 +8,31 @@ player::player(){
     jump_anim = LoadTexture("res/ja.png");
     crouch_anim = LoadTexture("res/caa.png");
     idle_block = LoadTexture("res/b.png");
+    punch_anim = LoadTexture("res/pa.png");
     frameRec = { 0.0f, 0.0f, (float)running_anim.width/8, (float)running_anim.height};
     position = {(float)GetScreenWidth()/4-200,400};
     speed = {900,0};
     health = 200, p_dmg = 10,k_dmg = 20, scale = 0.13f, groundY =220, keyframe = 200, Jumpheight = 0, crouchdepth = 0, crouch_y = 0;
     facingRight = true;
-    is_attacking = 0, is_blocking = 0, is_grounded = 0, is_upright = 0, is_moving = 0, just_dashed = 0, R_dash_allowed = 1, L_dash_allowed = 1;
+    is_punching = 0, is_blocking = 0, is_grounded = 0, is_upright = 0, is_moving = 0, just_dashed = 0, R_dash_allowed = 1, L_dash_allowed = 1, can_punch = 1;
 }
 
 
-void player::setControls(KeyboardKey up, KeyboardKey down, KeyboardKey left, KeyboardKey right){
-    Jump = up, crouch = down, Left = left, Right = right;
+void player::setControls(KeyboardKey up, KeyboardKey down, KeyboardKey left, KeyboardKey right, KeyboardKey punch){
+    Jump = up, crouch = down, Left = left, Right = right, Punch = punch;
 }
 
 
-void player::update_hitbox()
-{
+void player::update_hitbox(){
     hitbox = {position.x , position.y, (float)image.width*scale, (float)image.height*scale};
     if(facingRight){
         c_hitbox = {position.x + 50.0f, position.y + 80.0f + crouchdepth, (float)image.width*scale- 130.0f, ((float)image.height*scale/2.0f)-80.0f - crouchdepth};
         l_hitbox = {position.x + 40.0f , position.y + ((float)image.height*scale)/2.0f, (float)image.width*scale - 100.0f, (float)image.height*scale/2.0f + Jumpheight};
+        p_hitbox = {c_hitbox.x + c_hitbox.width, c_hitbox.y, ((hitbox.x + hitbox.width)-(c_hitbox.x + c_hitbox.width)), c_hitbox.height};
     }else if(!facingRight){
         c_hitbox = {position.x + 80.0f, position.y + 80.0f + crouchdepth, (float)image.width*scale- 130.0f, ((float)image.height*scale/2.0f)-80.0f - crouchdepth};
         l_hitbox = {position.x + 60.0f , position.y + ((float)image.height*scale)/2.0f, (float)image.width*scale - 100.0f, (float)image.height*scale/2.0f + Jumpheight};
+        p_hitbox = {hitbox.x, c_hitbox.y, c_hitbox.x - hitbox.x, c_hitbox.height};
     }
 }
 
@@ -47,6 +49,10 @@ void player::draw(){
         source = {200.0f*currentFrameJump, 0.0f, (float)jump_anim.width/8, (float)jump_anim.height};
         if (!facingRight) {source.width *= -1;}
         DrawTexturePro(jump_anim,source,destin,{0.0f, 0.0f}, 0.0f, RAYWHITE);
+    }else if(is_punching){
+        source = {200.0f*currentFrameJump, 0.0f, (float)punch_anim.width/8, (float)punch_anim.height};
+        if (!facingRight) {source.width *= -1;}
+        DrawTexturePro(punch_anim,source,destin,{0.0f, 0.0f}, 0.0f, RAYWHITE);
     }else if(just_dashed){
         source = {200.0f*currentFrameDash, 0.0f, (float)dash_anim.width/8, (float)dash_anim.height};
         if (!facingRight) {source.width *= -1;}
@@ -66,13 +72,15 @@ void player::draw(){
         if (!facingRight) {source.width *= -1;}
         DrawTexturePro(running_anim,source,destin,{0.0f, 0.0f}, 0.0f, RAYWHITE);
     }    
+
+    DrawText(TextFormat("%d", (int)health), hitbox.x, hitbox.y - 50, 50, RED);
     
-//    if(IsKeyDown(KEY_LEFT_CONTROL)){ 
+    if(IsKeyDown(KEY_LEFT_CONTROL)){ 
     DrawRectangleLinesEx(hitbox,2.0f,RED);
     DrawRectangleLinesEx(c_hitbox,2.0f,BLUE);
     DrawRectangleLinesEx(l_hitbox,2.0f,PURPLE);
-    if(is_blocking) DrawRectangleLines(hitbox.x + hitbox.width, hitbox.y, 10, hitbox.height/2.0f, GREEN);
-    // }
+    DrawRectangleLinesEx(p_hitbox, 2.0f, GREEN);
+    }
 }
 
 
@@ -107,10 +115,16 @@ void player::update(player &p2){
 
 //General Updates
     is_blocking = (facingRight && IsKeyDown(Left)) || (!facingRight && IsKeyDown(Right)) ?  1 : 0;
+    if(is_punching || !is_grounded || !is_upright){is_blocking = 0;}
     R_dash = 0, L_dash = 0, is_moving = 0;
-    framesCounter++;framesCounterDash++;framesCounterJump++;
+    framesCounter++;framesCounterDash++;framesCounterJump++; FCpun++;
     gravity_on();
 
+    if(!can_punch && FCpun >= 60/8){
+        FCpun = 0;
+        CFpun++;
+        if(CFpun > 13){CFpun = 0; can_punch = 1;}
+    }
 
 //Movement Frames
     //Movement
@@ -130,11 +144,11 @@ void player::update(player &p2){
     }
 
     //Jump and Crouch
-    if(is_grounded && is_upright){currentFrameJump = 0;}
-    if((!is_grounded || !is_upright) && (framesCounterJump >= (60/framesSpeedJump))){
+    if(is_grounded && is_upright && !is_punching){currentFrameJump = 0;}
+    if((!is_grounded || !is_upright || is_punching) && (framesCounterJump >= (60/framesSpeedJump))){
         framesCounterJump = 0;
         currentFrameJump++;
-        if (currentFrameJump > 7) {currentFrameJump = 0;}
+        if (currentFrameJump > 7) {is_punching =0, p_dmg_once=0, currentFrameJump = 0;}
         frameRec.x = (float)currentFrameJump*(float)jump_anim.width/8;
     }
 
@@ -151,7 +165,7 @@ void player::update(player &p2){
 
 //Movement
     //Movement allowed Check
-    if(!is_grounded || !is_upright){L_move_allowed = 0; R_move_allowed = 0, R_dash_allowed = 0, L_dash_allowed = 0;}
+    if(!is_grounded || !is_upright || is_punching){L_move_allowed = 0; R_move_allowed = 0, R_dash_allowed = 0, L_dash_allowed = 0;}
     //Dash
     if(!CheckCollisionRecs(hitbox, p2.hitbox)){L_input_times = 0; R_input_times = 0;}
     else{
@@ -169,16 +183,24 @@ void player::update(player &p2){
     if((IsKeyDown(Right) && !IsKeyDown(Left)) && R_move_allowed){position.x += speed.x * deltaTime;is_moving=1;}
     
 
+    if(((l_hitbox.y + l_hitbox.height) >= groundY) && (c_hitbox.y <= -285.0f) && !is_punching){R_dash_allowed = 1, L_dash_allowed = 1;}
 //Jump
-    if((l_hitbox.y + l_hitbox.height) >= groundY){is_grounded = 1, R_dash_allowed = 1, L_dash_allowed = 1;}else{is_grounded = 0;}
-    if(IsKeyPressed(Jump) && is_grounded && is_upright && !IsKeyDown(crouch)){speed.y = jumpVelocity;}
+    if((l_hitbox.y + l_hitbox.height) >= groundY){is_grounded = 1;}else{is_grounded = 0;}
+    if(IsKeyPressed(Jump) && is_grounded && is_upright && !IsKeyDown(crouch) && !is_punching){speed.y = jumpVelocity;}
     Jumpheight += speed.y * deltaTime;
 
 //Crouch
-    if(c_hitbox.y <= -285.0f){is_upright = 1, R_dash_allowed = 1, L_dash_allowed = 1;}else{is_upright  = 0;}
-    if(IsKeyPressed(crouch) && is_upright && is_grounded && !IsKeyDown(Jump)){crouch_y = crouchVelocity;}
+    if(c_hitbox.y <= -285.0f){is_upright = 1;}else{is_upright  = 0;}
+    if(IsKeyPressed(crouch) && is_upright && is_grounded && !IsKeyDown(Jump) && !is_punching){crouch_y = crouchVelocity;}
     crouchdepth += crouch_y * deltaTime;
 
+
+//Punch
+    if(IsKeyPressed(Punch) && is_grounded && is_upright && can_punch){is_punching = true; can_punch = 0;}
+    if(is_punching){
+        R_dash_allowed = 0, L_dash_allowed = 0;
+        if(CheckCollisionRecs(p_hitbox,p2.c_hitbox) && !p_dmg_once && !p2.is_blocking && p2.is_upright){p2.health -= p_dmg; p_dmg_once = 1;}
+    }
 
     update_hitbox();
 
@@ -195,7 +217,7 @@ void player::update(player &p2){
 Rectangle player::GetHitbox(){return hitbox;}
 Rectangle player::GetCHitbox(){return c_hitbox;}
 Rectangle player::GetLHitbox(){return l_hitbox;}
-KeyboardKey player::GetJump() { return Jump; }
+KeyboardKey player::GetJump(){ return Jump;}
 KeyboardKey player::GetCrouch(){return crouch;}
 KeyboardKey player::GetLeft(){return Left;}
 KeyboardKey player::GetRight(){return Right;}
@@ -208,4 +230,5 @@ player::~player(){
     UnloadTexture(jump_anim);
     UnloadTexture(crouch_anim);
     UnloadTexture(idle_block);
+    UnloadTexture(punch_anim);
 }
--- player.hpp
+++ player.hpp
@@ -3,14 +3,14 @@
 
 class player{
 private:
-    Rectangle hitbox, c_hitbox, l_hitbox, frameRec;
+    Rectangle hitbox, c_hitbox, l_hitbox, p_hitbox, frameRec;
 
-    KeyboardKey Jump, Left, Right, crouch;
+    KeyboardKey Jump, Left, Right, crouch, Punch;
 
-    Texture2D image, idle_block, running_anim,dash_anim, jump_anim, crouch_anim;
+    Texture2D image, idle_block, running_anim,dash_anim, jump_anim, crouch_anim, punch_anim;
     Vector2 speed;
     float health, p_dmg, k_dmg, scale, groundY, keyframe, L_input_times, R_input_times, Jumpheight, crouchdepth, crouch_y;
-    bool facingRight, is_attacking, is_blocking, is_grounded, is_upright, is_moving, just_dashed, R_dash_allowed, L_dash_allowed;
+    bool facingRight, is_punching, is_blocking, is_grounded, is_upright, is_moving, just_dashed, R_dash_allowed, L_dash_allowed, p_dmg_once, can_punch;
 
     const float gravity= 1200.0f;
     const float upwardsAcceleration = -1200.0f;
@@ -31,6 +31,8 @@ private:
     int framesCounterJump = 0;
     int framesSpeedJump = 8; 
 
+    int CFpun = 0, FCpun =0; 
+
 public:
 
     bool R_move_allowed, L_move_allowed, R_dash, L_dash;
@@ -42,7 +44,7 @@ public:
     void draw();
     void gravity_on();
     void update(player &p2);
-    void setControls(KeyboardKey up, KeyboardKey down, KeyboardKey left, KeyboardKey right);    
+    void setControls(KeyboardKey up, KeyboardKey down, KeyboardKey left, KeyboardKey right, KeyboardKey punch);    
     
     KeyboardKey GetJump();
     KeyboardKey GetCrouch();
